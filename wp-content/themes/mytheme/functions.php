7082e83bee6b1daa114f6ad96a7b1022851bba60
hideyo uesugi <hideyovic89yaogun@yahoo.co.jp>
2019-10-04 21:42:32 +0900

    style select added

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -37,10 +37,38 @@ add_action('pre_post_update', 'clear_ytwrapper');
 // アイキャッチ画像
 add_theme_support('post-thumbnails');
 
-// 編集画面の設定（h1を選択できなくする）
+// 編集画面の設定（h1を非選択、補足情報と注意書きのフォーマット追加）
 function editor_setting($init) {
 	$init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;Preformatted=pre';
+
+	$style_formats = array(
+		array(
+			'title' => '補足情報',
+			'block' => 'div',
+			'classes' => 'point'
+			'wrapper' => true
+		),
+		array(
+			'title' => '注意書き',
+			'block' => 'div',
+			'classes' => 'attention'
+		),
+		array(
+			'title' => 'ハイライト',
+			'inline' => 'span',
+			'classes' => 'highlight'
+		)
+	);
+	
+	$init['style_formats'] = json_encode($style_formats);
 	
 	return $init;
 }
 add_filter('tiny_mce_before_init', 'editor_setting');
+
+// スタイルメニューを有効化
+function add_stylemenu($buttons){
+	array_splice($buttons, 1, 0, 'styleselect');
+	return $buttons;
+}
+add_filter('mce_buttons_2', 'add_stylemenu');
