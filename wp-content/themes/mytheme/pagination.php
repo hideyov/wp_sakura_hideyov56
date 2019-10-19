4504b7ce7ee09152c9e3843cd23365a7d10ea040
hideyo uesugi <hideyovic89yaogun@yahoo.co.jp>
2019-10-19 22:16:44 +0900

    parts template pagination.php added

---
+++ wp-content/themes/mytheme/pagination.php
@@ -0,0 +1,8 @@
+<div class="pagination-single">
+	<p>
+		<span class="oldpage">
+			<?php previous_post_link(); ?></span>
+		<span class="newpage">
+			<?php next_post_link(); ?></span>
+	</p>
+</div>
--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -69,12 +69,7 @@
 				</ul>
 			</div>
 			
-			<div class="pagination-single">
-				<p>
-				<span class="oldpage"><?php previous_post_link(); ?></span>
-				<span class="newpage"><?php next_post_link(); ?></span>
-				</p>
-			</div>
+			<?php get_template_part('pagination'); ?>
 			
 			<?php if(has_category()) {
 				$cats = get_the_category();
