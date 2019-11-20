814c393f45409ccb08016b2c4669e1a1d116f531
hideyo uesugi <hideyovic89yaogun@yahoo.co.jp>
2019-11-20 16:20:14 +0900

    全ページのsidebar下部に目次メニュー追加

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -14,19 +14,27 @@ if($myposts): ?>
 		$post = get_post($post->object_id);
 		setup_postdata($post); ?>
 		<li><a href="<?php the_permalink(); ?>">
-			<div class="thumb" style="background-image:
+				<div class="thumb" style="background-image:
 			url(<?php echo mythumb('thumbnail'); ?>)"></div>
-			<div class="text">
-				<?php the_title(); ?>
-				<?php if(has_category()): ?>
-				<?php $postcat=get_the_category(); ?>
-				<span>
-					<?php echo $postcat[0]->name; ?></span>
-				<?php endif; ?>
-			</div>
+				<div class="text">
+					<?php the_title(); ?>
+					<?php if(has_category()): ?>
+					<?php $postcat=get_the_category(); ?>
+					<span>
+						<?php echo $postcat[0]->name; ?></span>
+					<?php endif; ?>
+				</div>
 			</a></li>
 		<?php endif; endforeach; ?>
 	</ul>
 </aside>
+
+<aside class="mymenu mymenu-page">
+	<h2>hideyov7</h2>
+	<?php wp_nav_menu(array(
+				'theme_location' => 'pagenav'
+			)); ?>
+</aside>
+
 <?php wp_reset_postdata();
 endif; ?>
